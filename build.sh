94e401250aaba11e9df06abfac9615f9b523cc66
Christina Maria Mayr <christinamariamayr@c51c059c-447a-499c-92d2-95abe18f09e0.fritz.box>
2025-12-26 12:40:26 +0100

    clean repo

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -R _build
-jupyter-book build .
-git add ./*
-git commit -m "update book content"
-git push origin master
-ghp-import -n -p -f _build/html
--- test.py
+++
@@ -1,2 +0,0 @@
-from jupyterquiz import display_quiz
-display_quiz(quiz, border_radius=2, lang='de')
